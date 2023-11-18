24efbe001f6cb93f23ac0ea353435825a4fbf58f
waynecharr <137343274+waynecharr@users.noreply.github.com>
2023-11-17 21:33:45 -0500

    added the html routes using app.get

--- README.md
+++ README.md
@@ -5,5 +5,6 @@
 - "initial commit, created server.js file nothing in it; installed uuid"
 - "started the initial work for the server.js to include the dependencies"
 - "added some assumed middleware as well as fb directory that I will be using. May make changes to these later or add more if needed"
+- "added the html routes using app.get"
 
 
--- server.js
+++ server.js
@@ -11,6 +11,15 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Get routes for the index and notes html. 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'indext.html'));
+});
+
 dbFilePath = path.join(__dirname, 'db', 'db.json');
 
 app.listen(PORT, function(){
