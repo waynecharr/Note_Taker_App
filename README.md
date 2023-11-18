76d55b5ea1e30eb43034b9c65b2826a9e7d81e82
waynecharr <137343274+waynecharr@users.noreply.github.com>
2023-11-17 21:28:25 -0500

    added some assumed middleware as well as fb directory that I will be using. May make changes to these later or add more if needed

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 
 - "initial commit, created server.js file nothing in it; installed uuid"
 - "started the initial work for the server.js to include the dependencies"
+- "added some assumed middleware as well as fb directory that I will be using. May make changes to these later or add more if needed"
+
+
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+dbFilePath = path.join(__dirname, 'db', 'db.json');
+
+app.listen(PORT, function(){
+    console.log('Server is listening on: http://localhost:' + PORT)
+ });
