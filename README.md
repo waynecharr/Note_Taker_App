6a9ebc23cb662446ca3a9b5979d8bd4e4b46a713
waynecharr <137343274+waynecharr@users.noreply.github.com>
2023-11-17 22:30:02 -0500

    updated the function for reading a file and created one for writing a file. Also updated the get function for the notes.

--- README.md
+++ README.md
@@ -7,5 +7,6 @@
 - "added some assumed middleware as well as fb directory that I will be using. May make changes to these later or add more if needed"
 - "added the html routes using app.get"
 - "added a get route for the notes. Also added a function for reading the notes and an array to capture them in."
+- "updated the function for reading a file and created one for writing a file. Also updated the get function for the notes." 
 
 
--- server.js
+++ server.js
@@ -3,46 +3,81 @@ const app = express();
 
 const fs = require('fs');
 const path = require('path');
+// Installed uuid to create a unique ID. 
 const { v4: uuidv4 } = require('uuid');
 
+// Port being used for listening. 
 const PORT = process.env.PORT || 3002;
 
+//middleware
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Get routes for the index and notes html. 
+// Get routes for the notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
+// Get routes from the index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+//Shortcute for the file path directory instead of using ./db/dbjson. Had errors when I used that.
 dbFilePath = path.join(__dirname, 'db', 'db.json');
 
-function notesUpdate(filePath, callback) {
+
+function readNotes(filePath, callback) {
     fs.readFile(filePath, 'utf-8', (err, data) => {
         if (err) {
             callback(err, null);
         } else {
-            const notesUpdated = JSON.parse(data);
-            callback(null, notesUpdated);
+            const notes = JSON.parse(data);
+            callback(null, notes);
         }
     });
 }
 
-notesUpdated = []
-
+function writeNotes(filePath, notes, callback) {
+    fs.writeFile(filePath, JSON.stringify(notes, null, 2), 'utf-8', (err) => {
+        if (err) {
+            callback(err);
+        } else {
+            callback(null);
+        }
+    });
+}
 
 app.get('/api/notes', (req, res) => {
-    notesUpdated(dbFilePath, (err, notes) => {
+    readNotes(dbFilePath, (err, notes) => {
         if (err) {
             console.error(err);
-            res.status(500).json({ error: 'Error reading or parsing data from the file.' });
+            res.status(500).json({ error: 'Unable to read the parsed data.' });
         } else {
             res.json(notes);
+        }
+    });
+});
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    newNote.id = uuidv4(); // Assign a unique ID using uuid
+
+    readNotes(dbFilePath, (err, notes) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Unable to read the parsed data.' });
+        } else {
+            notes.push(newNote);
+            writeNotes(dbFilePath, notes, (err) => {
+                if (err) {
+                    console.error(err);
+                    res.status(500).json({ error: 'Unable to write the parsed data.' });
+                } else {
+                    res.json(newNote);
+                }
+            });
         }
     });
 });
